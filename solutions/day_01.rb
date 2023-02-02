2e04cf120cb6882cf20ecfb7cbc22a025b7600cb
andrea uhr <andreakbuhr@gmail.com>
2023-02-02 16:52:28 -0500

    day 1 solution second half

--- solutions/day_01.rb
+++ solutions/day_01.rb
@@ -3,10 +3,19 @@ class Day01
     File.read('./inputs/day_01.txt')
   end
 
+  def elf_sum
+    data.split("\n\n").map { |elf| elf.split("\n").map(&:to_i).sum }
+  end
+
   def top_elf
-    data.split("\n\n").map { |elf| elf.split("\n").map(&:to_i).sum }.max
+    elf_sum.max
   end
 
   def top_three_elves
+    elf_sum.max(3).sum
   end 
 end 
+
+#require 'pry'
+#test = Day01.new
+#binding.pry
